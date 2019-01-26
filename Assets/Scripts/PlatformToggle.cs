b52d8320dc79a8b0b511f4127a6f496490c669c0
dugg everest <duggeverest@gmail.com>
2019-01-26 14:22:02 -0700

    Created script "PlatformToggle" and prefab "PlatformToggleSwitch" to turn on all platform objects when

---
+++ Assets/Scripts/PlatformToggle.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformToggle : MonoBehaviour
+{
+    private Renderer rend;
+    private BoxCollider2D col;
+
+    [SerializeField]
+    private GameObject[] platformObjects;
+
+    public bool platformToggleOn { get; set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       rend = GetComponent<Renderer>();
+       col = GetComponent<BoxCollider2D>();
+       rend.enabled = false;
+       col.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (platformToggleOn)
+      {
+         ActivatePlatformObjects();
+      }
+    }
+
+    // turn on all the platform objects in the scene
+    private void ActivatePlatformObjects()
+    {
+       for (int i = 0; i < platformObjects.Length; i++)
+       {
+          platformObjects[i].SetActive(true);
+       }
+    }
+}
