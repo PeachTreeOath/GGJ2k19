70203775ca4d23113a7983c19e1ba515c8c02fce
PeachTreeOath <peachtreeoath@gmail.com>
2019-01-26 16:22:11 -0700

    Adding feetbehavior

---
+++ Assets/Scripts/FeetBehavior.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class FeetBehavior : MonoBehaviour
+{
+    [SerializeField]
+    private PlayerController playerController;
+
+    void Start()
+    {
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        Debug.Log("EXIT");
+        if (collision.gameObject.tag == "Wall")
+        {
+                playerController.grounded = false;
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        Debug.Log("STAY");
+        if (collision.gameObject.tag == "Wall")
+        {
+            if (Math.Abs(playerController.rigidBody.velocity.y) <= playerController.groundedVDelta)
+            {
+                playerController.grounded = true;
+            }
+        }
+    }
+}
