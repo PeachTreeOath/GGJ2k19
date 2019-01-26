6f3dc858189689e8a7bba238127c1a0a3e1f2691
krevlee <krystalevlee@gmail.com>
2019-01-26 04:40:50 -0700

    Ignore everything... it's late

--- Assets/Scripts/FloatBehavior.cs
+++ Assets/Scripts/FloatBehavior.cs
@@ -4,33 +4,57 @@ using UnityEngine;
 
 public class FloatBehavior : MonoBehaviour
 {
+    // Time that object is floating without any sinking on top of the lava.
     [SerializeField]
     private float floatingTime;
 
     [SerializeField]
-    private float shrinkSpeed;
+    private float sinkSpeed;
 
     private BoxCollider2D collider;
 
     private Vector2 originalColliderSize;
 
-    private bool touchedLava;
+    [SerializeField]
+    private bool touchingLava;
+
+    // Very basic logic to create somewhat of a bobbing effect... should replace with actual bob logic.
+    [SerializeField]
+    private float sinkDelayTime;
+
+    [SerializeField]
+    private float currentSinkDelayTime;
 
     // Start is called before the first frame update
     void Start()
     {
         collider = GetComponentInChildren<BoxCollider2D>();
+        currentSinkDelayTime = sinkDelayTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (touchedLava)
+        if (touchingLava)
         {
-            floatingTime -= Time.deltaTime;
-            if (floatingTime <= 0)
+            if (floatingTime > 0)
             {
-                collider.size -= new Vector2(0, shrinkSpeed * Time.deltaTime);
+                floatingTime -= Time.deltaTime;
+            }
+            else
+            {
+                currentSinkDelayTime -= Time.deltaTime;
+                if (currentSinkDelayTime <= 0)
+                {
+                    float sinkSize = sinkSpeed * Time.deltaTime; 
+                    collider.offset += new Vector2(0, sinkSize/2);
+                    collider.size -= new Vector2(0, sinkSize);
+
+                    if (collider.size.y <= 0.001)
+                    {
+                        Destroy(gameObject);
+                    }
+                }
             }
         }
     }
@@ -45,7 +69,22 @@ public class FloatBehavior : MonoBehaviour
         LavaBehavior lava = other.gameObject.GetComponentInChildren<LavaBehavior>();
         if (lava != null)
         {
-            touchedLava = true;
+            touchingLava = true;
+        }
+    }
+
+    /// <summary>
+    /// Sent when a collider on another object stops touching this
+    /// object's collider (2D physics only).
+    /// </summary>
+    /// <param name="other">The Collision2D data associated with this collision.</param>
+    void OnCollisionExit2D(Collision2D other)
+    {
+        LavaBehavior lava = other.gameObject.GetComponentInChildren<LavaBehavior>();
+        if (lava != null)
+        {
+            touchingLava = false;
+            currentSinkDelayTime = sinkDelayTime;
         }
     }
 }
