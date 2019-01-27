2aba9bf37ef5844a4b4a1b6684bce66929d60367
Unknown <rfonger@gmail.com>
2019-01-26 17:58:58 -0700

    Adding death screen for phones

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using NDream.AirConsole;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -24,6 +25,7 @@ public class GameManager : Singleton<GameManager>
     void Start()
     {
         lava = FindObjectOfType<LavaBehavior>();
+        
     }
 
     // Update is called once per frame
@@ -52,5 +54,15 @@ public class GameManager : Singleton<GameManager>
         // Current player count
         Text currentPlayerText = currentPlayers.GetComponent<Text>();
         currentPlayerText.text = CURRENT_PLAYERS_STRING + players.Length;
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            AirConsole.instance.Broadcast("view:alive_view");
+        }
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            AirConsole.instance.Broadcast("view:dead_view");
+        }
     }
 }
