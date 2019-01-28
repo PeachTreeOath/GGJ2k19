b218b11ec320e84f0b988430ae44b297188c1fe9
PeachTreeOath <peachtreeoath@gmail.com>
2019-01-27 19:32:28 -0700

    final color changes

--- Assets/AirConsole/examples/platformer/PlatformerExampleLogic.cs
+++ Assets/AirConsole/examples/platformer/PlatformerExampleLogic.cs
@@ -22,7 +22,8 @@ public class PlatformerExampleLogic : MonoBehaviour
       new Color(0f/255f, 10f/255f, 17f/255f), // black
       new Color(255f/255f, 151f/255f, 197f/255f), // pink
       new Color(114f/255f, 114f/255f, 114f/255f), // grey
-      new Color(19f/255f, 74f/255f, 117f/255f),    // sea blue
+      new Color(83f/255f, 30f/255f, 255f/255f), // royal blue
+     
       new Color(113f/255f, 50f/255f, 12f/255f), // brown
       new Color(255f/255f, 174f/255f, 1f/255f), // orange
       new Color(245f/255f, 222f/255f, 179f/255f), // wheat
@@ -33,7 +34,7 @@ public class PlatformerExampleLogic : MonoBehaviour
       new Color(181f/255f, 78f/255f, 16f/255f), // brown red
       new Color(231f/255f, 187f/255f, 254f/255f), // lavender
       new Color(184f/255f, 229f/255f, 2f/255f), // sick green
-      new Color(83f/255f, 30f/255f, 255f/255f), // royal blue
+      
       new Color(3f/255f, 239f/255f, 164f/255f), // aqua green
       new Color(253f/255f, 206f/255f, 183f/255f), // pale orange
       new Color(97f/255f, 14f/255f, 175f/255f), // indigo
@@ -45,7 +46,8 @@ public class PlatformerExampleLogic : MonoBehaviour
       new Color(255f/255f, 254f/255f, 168f/255f), // pale yellow
       new Color(2f/255f, 171f/255f, 186f/255f), // teal green
       new Color(242f/255f, 132f/255f, 3f/255f), // brighter orange
-      new Color(192f/255f, 255f/255f, 141f/255f) // pale green
+      new Color(192f/255f, 255f/255f, 141f/255f), // pale green
+         new Color(19f/255f, 74f/255f, 117f/255f)    // sea blue
    };
 
     private int playerSpawnNumber = 0;
